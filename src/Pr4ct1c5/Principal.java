bcb49f108b6b3c9dc6b8580d7c877130682594ca
CasperDevs <casperdevs@outlook.es>
2023-03-02 01:01:43 -0600

    Pakage Pr4ct1c5 Added on POO2023

--- src/Pr4ct1c5/Principal.java
+++ src/Pr4ct1c5/Principal.java
@@ -1,4 +1,22 @@
 package Pr4ct1c5;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Principal {
-}
+
+    public static void main(String[] args) {
+        System.out.println("Hola mondo");
+
+        Saludar();
+
+    }
+
+    public static void Saludar(){}
+
+
+        System.out.printf("Hola desde metodo saludar");
+    }
+
+
+
+
